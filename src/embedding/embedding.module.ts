91db3215456758e8ac9716556a07fe59ace4ff4d
Ksum7 <xamchik02@gmail.com>
2025-10-19 23:48:39 +0300

    fix providers

--- src/embedding/embedding.module.ts
+++ src/embedding/embedding.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
 import { HttpModule } from '@nestjs/axios';
 import { OllamaService } from './ollama.service';
+import { EmbeddingRepository } from './embedding.repository';
 
 @Module({
   imports: [ConfigModule, PrismaModule, HttpModule],
-  providers: [OllamaService],
-  exports: [OllamaService],
+  providers: [OllamaService, EmbeddingRepository],
+  exports: [OllamaService, EmbeddingRepository],
 })
 export class EmbeddingModule {}
