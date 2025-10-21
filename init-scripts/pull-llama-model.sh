e5451e63fcfd7cd07878d418f2ed0b735847d78f
nezhinskii <maksimnezhin@gmail.com>
2025-10-21 09:24:53 +0300

    updated ollama docker-compose

--- init-scripts/pull-llama-model.sh
+++ init-scripts/pull-llama-model.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e  # Остановка при ошибке
-
-echo "Installing model: $MODEL_NAME"
-ollama pull "$MODEL_NAME"
-
-if ollama list | grep -q "$MODEL_NAME"; then
-  echo "Model $MODEL_NAME installed successfully."
-else
-  echo "Failed to install model $MODEL_NAME."
-  exit 1
-fi
-
 echo "Starting Ollama server..."
-exec ollama serve
+ollama serve & 
+sleep 5
+for model in $MODEL_NAMES; do
+    echo "Installing model: $model"
+    ollama pull "$model"
+
+    if ollama list | grep -q "$model"; then
+      echo "Model $model installed successfully."
+    else
+      echo "Failed to install model $model."
+      exit 1
+    fi
+done
+wait
