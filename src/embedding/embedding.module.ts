5cdc6972f270a43924e47ca0ecb2a927c0bbb538
Ksum7 <xamchik02@gmail.com>
2025-11-04 00:40:57 +0300

    Update embedding.module.ts

--- src/embedding/embedding.module.ts
+++ src/embedding/embedding.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../prisma/prisma.module';
-import { HttpModule } from '@nestjs/axios';
-import { OllamaService } from '../ollama/ollama.service';
 import { EmbeddingRepository } from './embedding.repository';
-import { OllamaModule } from 'src/ollama/ollama.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, HttpModule, OllamaModule],
+  imports: [PrismaModule],
   providers: [EmbeddingRepository],
   exports: [EmbeddingRepository],
 })
